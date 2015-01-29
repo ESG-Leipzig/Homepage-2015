4c9915a71d82eba58f0b9124ab8f7c7af23fe6f6
Norman Jäckel <esg-leipzig-homepage-2015@normanjaeckel.de>
2015-01-29 15:12:23 +0100

    Updated flatpage model.

--- esg_leipzig_homepage_2015/models.py
+++ esg_leipzig_homepage_2015/models.py
@@ -1,35 +1,40 @@
+from django.core.urlresolvers import reverse
 from django.db import models
-from django.core.urlresolvers import get_script_prefix
-from django.utils.encoding import iri_to_uri
 from django.utils.translation import ugettext_lazy
 
 
 class FlatPage(models.Model):
     """
-    TODO
+    Model for static pages.
     """
-    slug = models.CharField(
-        ugettext_lazy('Slug'),
-        max_length=255,
-        db_index=True,
-        help_text=ugettext_lazy(''))
+    slug = models.SlugField(
+        ugettext_lazy('Slug/URL'),
+        help_text=ugettext_lazy(
+            "Beispiel: 'impressum'. Keinen voran- oder nachgestellten "
+            "Schrägstrich setzen."))
     title = models.CharField(
         ugettext_lazy('Titel'),
         max_length=100,
-        help_text=ugettext_lazy(''))
+        help_text=ugettext_lazy(
+            "Beispiel: 'Impressum'. Der Titel wird als Link in den Menüs "
+            "angezeigt."))
     content = models.TextField(
-        ugettext_lazy('Inhalt'),
+        ugettext_lazy('Inhalt (HTML)'),
         blank=True,
-        help_text=ugettext_lazy(''))
+        help_text=ugettext_lazy("Beispiel: '<div></div>'."))
     weight = models.IntegerField(
         ugettext_lazy('Gewicht'),
-        default=0,
-        help_text=ugettext_lazy(''))
+        default=100,
+        help_text=ugettext_lazy(
+            'Eine höhere Zahl bedeutet, dass der Eintrag im Menü weiter '
+            'unten steht.'))
     template_name = models.CharField(
         ugettext_lazy('Vorlage'),
-        max_length=70,
+        max_length=255,
         blank=True,
-        help_text=ugettext_lazy("Beispiel: 'meine_seite.html'. Wenn nicht anders angegeben wird 'flatpage_default.html' verwendet."))
+        help_text=ugettext_lazy(
+            "Beispiel: 'meine_seite.html'. Wenn nicht anders angegeben wird "
+            "die Vorlage 'flatpage_default.html' verwendet."))
 
     class Meta:
         ordering = ('weight', 'slug',)
@@ -37,9 +42,7 @@ class FlatPage(models.Model):
         verbose_name_plural = ugettext_lazy('Statische Seiten')
 
     def __str__(self):
-        return "%s -- %s" % (self.slug, self.title)
+        return self.title
 
     def get_absolute_url(self):
-        # TODO
-        # Handle script prefix manually because we bypass reverse()
-        return iri_to_uri(get_script_prefix().rstrip('/') + self.slug + '/')
+        return reverse('flatpage', args=[self.slug])
