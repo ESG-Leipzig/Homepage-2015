9ebd7caf4c19d3058267671b3a081206a3672e4b
Norman Jäckel <esg-leipzig-homepage-2015@normanjaeckel.de>
2015-02-01 21:34:31 +0100

    Fixed isort tasks.

--- esg_leipzig_homepage_2015/default_wsgi.py
+++ esg_leipzig_homepage_2015/default_wsgi.py
@@ -7,7 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os, site
+import os
+
+# import site
 
 DJANGO_SETTINGS_MODULE = ''
 
